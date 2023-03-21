c14fb0d4b14f274c34733d8d3bbe371c73596c94
huahuadan <ahuahuadan@qq.com>
2023-03-21 18:46:25 +0800

    feat(Resource):add ram,cpu,storage,cpu models

---
+++ src/main/java/org/cloudsimplus/resources/Cpu.java
@@ -0,0 +1,7 @@
+package org.cloudsimplus.resources;
+
+public final class Cpu  extends ResourceManageableAbstract {
+    public Cpu(final long capacity) {
+        super(capacity, "unit");
+    }
+}
