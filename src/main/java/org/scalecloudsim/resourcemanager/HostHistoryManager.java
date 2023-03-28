9ed7ed30f393d8f426cbcb850bdab237324db62e
huahuadan <ahuahuadan@qq.com>
2023-03-28 16:23:52 +0800

    feat(resourcemanager):add getSpecialTimeHostState function

--- src/main/java/org/scalecloudsim/resourcemanager/HostHistoryManager.java
+++ src/main/java/org/scalecloudsim/resourcemanager/HostHistoryManager.java
@@ -5,17 +5,11 @@ import org.slf4j.LoggerFactory;
 
 public interface HostHistoryManager{
     Logger LOGGER = LoggerFactory.getLogger(HostHistoryManager.class.getSimpleName());
-
     HostHistoryManager setHistoryRange(double range);
-
     double getHistoryRange();
-
     HostResourceState addDelayWatch(double delayTime);
     HostHistoryManager delDelayWatch(double delayTime);
-
     HostHistoryManager updateHistory(double clock);
     HostHistoryManager addHistory(HostResourceStateHistory hostResourceStateHistory);
-
-    HostResourceState getSpecialTimeHostState(double delayTime);
-
+    HostResourceState getSpecialTimeHostState(double time);
 }
