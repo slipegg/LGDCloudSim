779dc41761817bcf69458f008424ab75961a7ce5
huahuadan <ahuahuadan@qq.com>
2023-03-28 16:37:19 +0800

    feat(resourcemanager):Support for deleting watch points in an partition

--- src/main/java/org/scalecloudsim/resourcemanager/PartitionStateManagerSimple.java
+++ src/main/java/org/scalecloudsim/resourcemanager/PartitionStateManagerSimple.java
@@ -5,10 +5,7 @@ import org.scalecloudsim.datacenters.Datacenter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PartitionStateManagerSimple implements PartitionStateManager{
     Logger LOGGER = LoggerFactory.getLogger(PartitionStateManagerSimple.class.getSimpleName());
@@ -17,11 +14,17 @@ public class PartitionStateManagerSimple implements PartitionStateManager{
     Map<Double,List<HostResourceState>> partitionState;
     public PartitionStateManagerSimple(StateManager stateManager,PartitionRange partitionRange){
         this.stateManager=stateManager;
-        this.partitionRange=partitionRange;
+        setPartitionRange(partitionRange);
         partitionState=new HashMap<>();
     }
     @Override
     public PartitionStateManager setPartitionRange(PartitionRange partitionRange) {
+        if(this.partitionRange!=partitionRange){
+            LOGGER.info("the partition range has been set,we will change the partition range and the watch will be deleted");
+            for(Map.Entry<Double,List<HostResourceState>> entry:partitionState.entrySet()){
+                delDelayWatch(entry.getKey());
+            }
+        }
         this.partitionRange=partitionRange;
         return this;
     }
@@ -42,6 +45,33 @@ public class PartitionStateManagerSimple implements PartitionStateManager{
             HostHistoryManager hostHistoryManager= host.getHostHistoryManager();
             HostResourceState hostResourceState = hostHistoryManager.addDelayWatch(delay);
             partitionDelayState.add(hostResourceState);
+        }
+        return this;
+    }
+
+    @Override
+    public PartitionStateManager delDelayWatch(double delay) {
+//        List<HostResourceState> partitionDelayState=new ArrayList<>();
+//        partitionState.put(delay,partitionDelayState);
+        //注意for循环需要优化
+        List<Host> hosts=stateManager.getDatacenter().getHostList();
+        for(int i=partitionRange.getStartIndex();i<= partitionRange.getEndIndex();i++){
+            Host host=hosts.get(i);
+            HostHistoryManager hostHistoryManager= host.getHostHistoryManager();
+            hostHistoryManager.delDelayWatch(delay);
+        }
+        partitionState.remove(delay);
+        return this;
+    }
+
+    @Override
+    public PartitionStateManager delAllDelayWatch() {
+        if(!partitionState.isEmpty())
+        {
+            Set<Double> keys=partitionState.keySet();
+            while (!keys.isEmpty()){
+                delDelayWatch(keys.iterator().next());
+            }
         }
         return this;
     }
