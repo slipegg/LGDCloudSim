5aed490732bdc1b4ad8a04c850b19e32476b296b
huahuadan <ahuahuadan@qq.com>
2023-04-05 12:34:27 +0800

    feat(core):add SimEntity model

--- src/main/java/org/cloudsimplus/core/SimEntity.java
+++ src/main/java/org/cloudsimplus/core/SimEntity.java
@@ -8,21 +8,32 @@ package org.cloudsimplus.core;
 
 //import org.cloudbus.cloudsim.core.events.SimEvent;
 
+import lombok.NonNull;
+import org.cloudsimplus.core.events.SimEvent;
 /**
  * An interface that represents a simulation entity. An entity handles events and can
  * send events to other entities.
  *
  * @author Marcos Dias de Assuncao
  * @author Manoel Campos da Silva Filho
- * @see CloudSimEntity
  * @since CloudSim Plus 1.0
  */
 public interface SimEntity extends Nameable, Runnable, Comparable<SimEntity> {
     /**
      * Defines the event state.
      */
+
+    SimEntity NULL = new SimEntityNull();//(SimEntityNullBase) (comparable) -> 0;
     enum State {RUNNABLE, WAITING, HOLDING, FINISHED}
     SimEntity setName(String newName) throws IllegalArgumentException;
+//    SimEntity setSimulation(Simulation simulation);
+    Simulation getSimulation();
+    boolean start();
+    @Override void run();
+    boolean schedule(SimEvent evt);
+    boolean schedule(SimEntity dest, double delay, int tag);
+    boolean schedule(SimEntity dest, double delay, int tag, Object data);
+    void processEvent(SimEvent evt);
 //
 //    /**
 //     * An attribute that implements the Null Object Design Pattern for {@link SimEntity}
