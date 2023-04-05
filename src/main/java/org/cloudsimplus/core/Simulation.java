52d98f86360c29052fb0a91d0335b96a6daf19e1
huahuadan <ahuahuadan@qq.com>
2023-04-05 12:36:25 +0800

    feat(core):add new Simulation model and its simple Cloudsim

--- src/main/java/org/cloudsimplus/core/Simulation.java
+++ src/main/java/org/cloudsimplus/core/Simulation.java
@@ -1,9 +1,21 @@
 package org.cloudsimplus.core;
 
 import org.cloudsimplus.core.SimulationNull;
+import org.cloudsimplus.core.events.SimEvent;
+
+import java.util.function.Predicate;
 
 public interface Simulation {
     Simulation NULL=new SimulationNull();
     double clock();
+    String clockStr();
     Simulation setClock(double time);
+    void addEntity(CloudSimEntity entity);
+    SimEvent select(SimEntity dest, Predicate<SimEvent> predicate);
+    SimEvent findFirstDeferred(SimEntity dest, Predicate<SimEvent> predicate);
+    void send(SimEvent evt);
+    CloudInformationService getCis();
+    double start();
+    void startSync();
+    int getNumEntities();
 }
