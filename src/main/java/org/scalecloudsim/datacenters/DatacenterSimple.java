0fe10e515502dcc398dc1920672eb4a57b7dbab9
huahuadan <ahuahuadan@qq.com>
2023-04-05 17:00:38 +0800

    feat(datacentures):add function to process UserRequestsSend

--- src/main/java/org/scalecloudsim/datacenters/DatacenterSimple.java
+++ src/main/java/org/scalecloudsim/datacenters/DatacenterSimple.java
@@ -6,8 +6,8 @@ import org.cloudsimplus.core.CloudSimTag;
 import org.cloudsimplus.core.SimEntity;
 import org.cloudsimplus.core.Simulation;
 import org.cloudsimplus.core.events.SimEvent;
+import org.scalecloudsim.Instances.InstanceGroup;
 import org.scalecloudsim.Instances.UserRequest;
-import org.scalecloudsim.datacenters.Datacenter;
 import org.scalecloudsim.statemanager.StateManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +20,7 @@ import java.util.Set;
 public class DatacenterSimple extends CloudSimEntity implements Datacenter{
     public Logger LOGGER = LoggerFactory.getLogger(DatacenterSimple.class.getSimpleName());
     private Set<Integer> collaborationIds;
-
+    private GroupQueue groupQueue;
     /**
      * Creates a new entity.
      *
@@ -30,10 +30,10 @@ public class DatacenterSimple extends CloudSimEntity implements Datacenter{
     public DatacenterSimple(@NonNull Simulation simulation) {
         super(simulation);
         this.collaborationIds = new HashSet<>();
+        this.groupQueue = new GroupQueueFifo();
     }
     public DatacenterSimple(@NonNull Simulation simulation,int id) {
-        super(simulation);
-        this.collaborationIds = new HashSet<>();
+        this(simulation);
         this.setId(id);
     }
     @Override
@@ -81,14 +81,37 @@ public class DatacenterSimple extends CloudSimEntity implements Datacenter{
     public void processEvent(SimEvent evt) {
         switch (evt.getTag()) {
             case CloudSimTag.USER_REQUEST_SEND -> processUserRequestsSend(evt);
-            default -> LOGGER.warn("{}: {} received unknown event {}", getSimulation().clockStr(), getName(), evt.getTag());
+            case CloudSimTag.INTER_SCHEDULE -> processInterSchedule();
+            default ->
+                    LOGGER.warn("{}: {} received unknown event {}", getSimulation().clockStr(), getName(), evt.getTag());
         }
     }
 
     private void processUserRequestsSend(final SimEvent evt) {
-        if(evt.getData() instanceof List<?> userRequests){
-            LOGGER.info("{}: {} received {} user request", getSimulation().clockStr(), getName(),userRequests.size());
+        if(evt.getData() instanceof List<?> userRequests) {
+            for (Object userRequest : userRequests) {
+                if (userRequest instanceof UserRequest) {
+                    groupQueue.addInstanceGroups((UserRequest) userRequest);
+                }
+            }
+            LOGGER.info("{}: {} received {} user request.The size of InstanceGroup queue is {}.", getSimulation().clockStr(), getName(), userRequests.size(), groupQueue.getGroupNum());
         }
+        sendNow(this, CloudSimTag.INTER_SCHEDULE);
+    }
+
+    private void processInterSchedule() {
+        List<InstanceGroup> instanceGroups = groupQueue.getInstanceGroups();
+        //得到其他数据中心的基础信息和资源抽样信息
+
+        //进行域间调度
+        /*
+         * 1.根据接入时延要求得到可调度的数据中心
+         * 2.根据资源抽样信息得到可调度的数据中心
+         * 3.根据网络时延和宽带情况以及抽样信息得到最优的调度方案
+         * 4.如果亲和组只有一个调度方案直接发送过去继续调度
+         * 4.如果没有可以的调度方案就将其返回给亲和组队列
+         * 4.一个亲和组有多个可调度的数据中心，询问数据中心能否进行调度
+         */
     }
 
     @Override
