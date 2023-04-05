fe175abb01889ddee7d199f0919ea8c7c41510af
huahuadan <ahuahuadan@qq.com>
2023-04-05 12:32:25 +0800

    feat(core):add CloudSimTag model

--- src/main/java/org/cloudsimplus/core/CloudSimTag.java
+++ src/main/java/org/cloudsimplus/core/CloudSimTag.java
@@ -1,9 +1,19 @@
 package org.cloudsimplus.core;
 
-public class CloudSimTag {
-    public static final int NONE=-1;
-    public static final int NORMAL=0;
-    public static final int AFFINIT=1;
-    public static final int NON_AFFINIT=2;
+public class CloudSimTag{
+    public static final int NONE = -1;
 
+    /** Starting constant value for cloud-related tags. */
+    private static final int BASE = 0;
+    public static final int DC_REGISTRATION_REQUEST=BASE+1;
+    public static final int DC_LIST_REQUEST =BASE+2;
+    public static final int USER_REQUEST_SEND =BASE+3;//给dc发送的用户请求
+    public static final int SEND_USER_REQUEST =BASE+4;//USER发送用户请求
+    private final int priority;
+    public int priority(){
+        return priority;
+    }
+    CloudSimTag() {
+        this.priority = 0;
+    }
 }
