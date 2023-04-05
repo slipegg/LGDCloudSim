05ce4d32cf8c8ea2eedba3fb3b6a70e8f52f1107
huahuadan <ahuahuadan@qq.com>
2023-04-05 23:59:44 +0800

    feat(statemanager):add SimpleState function and its simple

---
+++ src/main/java/org/scalecloudsim/statemanager/SimpleState.java
@@ -0,0 +1,27 @@
+package org.scalecloudsim.statemanager;
+
+import org.apache.commons.lang3.mutable.MutableInt;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SimpleState {
+    SimpleState updateStorageSum(int changeStorage);
+
+    SimpleState updateBwSum(int changeBw);
+
+    SimpleState addCpuRamRecord(int cpu, int ram);
+
+    SimpleState updateCpuRamMap(int originCpu, int originRam, int nowCpu, int nowRam);
+
+    long getStorageSum();
+
+    long getBwSum();
+
+    Map<Integer, Map<Integer, MutableInt>> getCpuRamMap();
+
+    int getCpuRamSum(int cpu, int ram);
+
+    List<List<Integer>> getCpuRamItem();
+
+}
---
+++ src/main/java/org/scalecloudsim/statemanager/SimpleStateSimple.java
@@ -0,0 +1,190 @@
+package org.scalecloudsim.statemanager;
+
+import ch.qos.logback.core.joran.sanity.Pair;
+import lombok.Getter;
+import org.apache.commons.lang3.mutable.MutableInt;
+
+import java.util.*;
+
+@Getter
+public class SimpleStateSimple implements SimpleState {
+    long storageSum;
+    long bwSum;
+    Map<Integer, Map<Integer, MutableInt>> cpuRamMap;
+    Map<Integer, Map<Integer, List<MutableInt>>> cpuRamSumMap;
+
+    public SimpleStateSimple() {
+        this.storageSum = 0;
+        this.bwSum = 0;
+        this.cpuRamMap = new TreeMap<>(Comparator.reverseOrder());
+        this.cpuRamSumMap = new TreeMap<>(Comparator.reverseOrder());
+
+        List<Integer> cpuRecordList = getCpuRecordList();
+        List<Integer> ramRecordList = getRamRecordList();
+        for (int cpu : cpuRecordList) {
+            Map<Integer, MutableInt> ramMap = new TreeMap<>(Comparator.reverseOrder());
+            Map<Integer, List<MutableInt>> ramSumMap = new TreeMap<>(Comparator.reverseOrder());
+            cpuRamMap.put(cpu, ramMap);
+            cpuRamSumMap.put(cpu, ramSumMap);
+            for (int ram : ramRecordList) {
+                MutableInt mutableInt = new MutableInt(0);
+                ramMap.put(ram, mutableInt);
+                List<MutableInt> mutableIntList = new ArrayList<>();
+                ramSumMap.put(ram, mutableIntList);
+                for (int smallerCpu : cpuRecordList) {
+                    if (smallerCpu > cpu) {
+                        break;
+                    }
+                    for (int smallerRam : ramRecordList) {
+                        if (smallerRam > ram) {
+                            break;
+                        }
+                        cpuRamSumMap.get(smallerCpu).get(smallerRam).add(mutableInt);
+                    }
+                }
+            }
+        }
+//        for(Map.Entry<Integer, Map<Integer, MutableInt>> cpuItem:cpuRamMap.entrySet()){
+//            for(Map.Entry<Integer, MutableInt> ramItem:cpuItem.getValue().entrySet()){
+//                for(Map.Entry<Integer, Map<Integer, MutableInt>> smallerCpuItem:cpuRamMap.entrySet()){
+//                    if(smallerCpuItem.getKey()>cpuItem.getKey()){
+//                        break;
+//                    }
+//                    for(Map.Entry<Integer, MutableInt> smallerRamItem:smallerCpuItem.getValue().entrySet()){
+//                        if(smallerRamItem.getKey()>ramItem.getKey()){
+//                            break;
+//                        }
+//                        cpuRamSumMap.get(cpuItem.getKey()).get(ramItem.getKey()).add(smallerRamItem.getValue());
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    private List<Integer> getCpuRecordList() {
+        List<Integer> cpuRecordList = new ArrayList<>();
+        int cpuMax = 128;
+        cpuRecordList.add(1);
+        for (int i = 2; i < 32; i += 2) {
+            cpuRecordList.add(i);
+        }
+        for (int i = 32; i < 64; i += 4) {
+            cpuRecordList.add(i);
+        }
+        for (int i = 64; i <= cpuMax; i += 8) {
+            cpuRecordList.add(i);
+        }
+        /*
+         * 实际返回：长度33，[1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 88, 96, 104, 112, 120, 128]
+         * */
+        return cpuRecordList;
+    }
+
+    private List<Integer> getRamRecordList() {
+        List<Integer> ramRecordList = new ArrayList<>();
+        int ramMax = 1024;
+        ramRecordList.add(1);
+        for (int i = 2; i < 32; i += 2) {
+            ramRecordList.add(i);
+        }
+        for (int i = 32; i < 128; i += 4) {
+            ramRecordList.add(i);
+        }
+        for (int i = 128; i <= 256; i += 16) {
+            ramRecordList.add(i);
+        }
+        for (int i = 256; i <= ramMax; i += 128) {
+            ramRecordList.add(i);
+        }
+        /*
+         * 实际返回：长度56，[1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128, 144, 160, 176, 192, 208, 224, 240, 256, 256, 384, 512, 640, 768, 896, 1024]
+         * */
+        return ramRecordList;
+    }
+
+    @Override
+    public SimpleState updateStorageSum(int changeStorage) {
+        storageSum += changeStorage;
+        return this;
+    }
+
+    @Override
+    public SimpleState updateBwSum(int changeBw) {
+        bwSum += changeBw;
+        return this;
+    }
+
+    @Override
+    public SimpleState addCpuRamRecord(int cpu, int ram) {
+        int smallerCpu = getSmallerCpu(cpu);
+        int smallerRam = getSmallerRam(cpu, ram);
+        cpuRamMap.get(smallerCpu).get(smallerRam).increment();
+        return this;
+    }
+
+    @Override
+    public SimpleState updateCpuRamMap(int originCpu, int originRam, int nowCpu, int nowRam) {
+        int smallerOriginCpu = getSmallerCpu(originCpu);
+        int smallerOriginRam = getSmallerRam(originCpu, originRam);
+        int smallerNowCpu = getSmallerCpu(nowCpu);
+        int smallerNowRam = getSmallerRam(nowCpu, nowRam);
+
+        cpuRamMap.get(smallerOriginCpu).get(smallerOriginRam).decrement();
+        cpuRamMap.get(smallerNowCpu).get(smallerNowRam).increment();
+        return this;
+    }
+
+    private int getSmallerCpu(int cpu) {
+        if (cpuRamMap.containsKey(cpu)) {
+            return cpu;
+        } else {
+            return cpuRamMap.keySet().stream().filter(key -> key <= cpu).findFirst().orElse(0);
+        }
+    }
+
+    private int getSmallerRam(int cpu, int ram) {
+        if (cpuRamMap.get(cpu).containsKey(ram)) {
+            return ram;
+        } else {
+            return cpuRamMap.get(cpu).keySet().stream().filter(key -> key <= ram).findFirst().orElse(0);
+        }
+    }
+
+    @Override
+    public int getCpuRamSum(int cpu, int ram) {
+        int biggerCpu = getBiggerCpu(cpu);
+        int biggerRam = getBiggerRam(biggerCpu, ram);
+        List<MutableInt> mutableIntList = cpuRamSumMap.get(biggerCpu).get(biggerRam);
+        return mutableIntList.stream().mapToInt(MutableInt::intValue).sum();
+    }
+
+    private int getBiggerCpu(int cpu) {
+        if (cpuRamMap.containsKey(cpu)) {
+            return cpu;
+        } else {
+            return cpuRamMap.keySet().stream().filter(key -> key >= cpu).findFirst().orElse(0);
+        }
+    }
+
+    private int getBiggerRam(int cpu, int ram) {
+        if (cpuRamMap.get(cpu).containsKey(ram)) {
+            return ram;
+        } else {
+            return cpuRamMap.get(cpu).keySet().stream().filter(key -> key >= ram).findFirst().orElse(0);
+        }
+    }
+
+    @Override
+    public List<List<Integer>> getCpuRamItem() {
+        List<List<Integer>> cpuRamItemList = new ArrayList<>();
+        for (Map.Entry<Integer, Map<Integer, MutableInt>> cpuItem : cpuRamMap.entrySet()) {
+            for (Integer ram : cpuItem.getValue().keySet()) {
+                List<Integer> cpuRamItem = new ArrayList<>();
+                cpuRamItem.add(cpuItem.getKey());
+                cpuRamItem.add(ram);
+                cpuRamItemList.add(cpuRamItem);
+            }
+        }
+        return cpuRamItemList;
+    }
+}
