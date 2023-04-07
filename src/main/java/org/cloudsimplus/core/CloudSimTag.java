c51b42e4547bd2d7f9bf72361ddba3be77420951
huahuadan <ahuahuadan@qq.com>
2023-04-07 21:42:01 +0800

    feat(core):add ASK_DC_REVIVE_GROUP tag

--- src/main/java/org/cloudsimplus/core/CloudSimTag.java
+++ src/main/java/org/cloudsimplus/core/CloudSimTag.java
@@ -12,6 +12,7 @@ public class CloudSimTag {
     public static final int USER_REQUEST_SEND = BASE + 3;//需要dc处理接收到的用户请求
     public static final int SEND_USER_REQUEST = BASE + 4;//USER发送用户请求给dc
     public static final int INTER_SCHEDULE = BASE + 5;//USER发送用户请求,需要比USER_REQUEST_SEND小
+    public static final int ASK_DC_REVIVE_GROUP = BASE + 6;
     private final int priority;
 
     public int priority() {
