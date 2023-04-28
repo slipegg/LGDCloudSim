95156ee2756603d1b0e28e5fc64fe788bb8f835c
huahuadan <ahuahuadan@qq.com>
2023-04-28 16:46:30 +0800

    feat(record):record submit time

--- src/main/java/org/scalecloudsim/record/CsvRecord.java
+++ src/main/java/org/scalecloudsim/record/CsvRecord.java
@@ -34,7 +34,7 @@ public class CsvRecord {
             writer = new BufferedWriter(new FileWriter(this.filePath));
             // 写入表头
             printer = new CSVPrinter(writer, CSVFormat.Builder.create()
-                    .setHeader("Instance", "InstanceGroup", "UserRequest", "cpu", "ram", "storage", "bandwidth", "lifetime", "datacenter", "host", "start time", "end time", "retry num", "state", "fail_reason")
+                    .setHeader("Instance", "InstanceGroup", "UserRequest", "cpu", "ram", "storage", "bandwidth", "lifetime", "datacenter", "host", "submit time", "start time", "end time", "retry num", "state", "fail_reason")
                     .build());
         } catch (IOException e) {
             e.printStackTrace();
@@ -67,7 +67,7 @@ public class CsvRecord {
         try {
             printer.printRecord(instance.getId(), instance.getInstanceGroup().getId(), instance.getUserRequest().getId(),
                     instance.getCpu(), instance.getRam(), instance.getStorage(), instance.getBw(), instance.getLifeTime(),
-                    dcId, instance.getHost(), instance.getStartTime(), instance.getFinishTime(), instance.getRetryNum(), UserRequest.stateToString(instance.getState()), instance.getUserRequest().getFailReason());
+                    dcId, instance.getHost(), instance.getUserRequest().getSubmitTime(), instance.getStartTime(), instance.getFinishTime(), instance.getRetryNum(), UserRequest.stateToString(instance.getState()), instance.getUserRequest().getFailReason());
 
         } catch (IOException e) {
             e.printStackTrace();
