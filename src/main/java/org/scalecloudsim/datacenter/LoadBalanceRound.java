08e14ad9a56c7ea5bce3f31dfa6f5eed6fa9c486
huahuadan <ahuahuadan@qq.com>
2023-05-10 20:05:03 +0800

    feat(datacenter):Distribute all instances evenly to the queues of each scheduler every time

--- src/main/java/org/scalecloudsim/datacenter/LoadBalanceRound.java
+++ src/main/java/org/scalecloudsim/datacenter/LoadBalanceRound.java
@@ -5,7 +5,10 @@ import lombok.Setter;
 import org.scalecloudsim.request.Instance;
 import org.scalecloudsim.innerscheduler.InnerScheduler;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.apache.commons.lang3.math.NumberUtils.max;
 
 public class LoadBalanceRound implements LoadBalance {
     @Getter
@@ -19,10 +22,27 @@ public class LoadBalanceRound implements LoadBalance {
 
     @Override
     public List<InnerScheduler> sendInstances(List<Instance> instances) {
-        InnerScheduler innerScheduler = datacenter.getInnerSchedulers().get(lastInnerSchedulerId);
-        innerScheduler.addInstance(instances);
+        List<InnerScheduler> sentInnerSchedulers = new ArrayList<>();
+        int size = instances.size();
+        List<InnerScheduler> innerSchedulers = datacenter.getInnerSchedulers();
+        int onceSendSize = size / innerSchedulers.size();
+        int remainder = size % innerSchedulers.size();
+
+        int start = 0;
+        int end;
+        for (int i = 0; i < innerSchedulers.size(); i++) {
+            InnerScheduler innerScheduler = innerSchedulers.get((lastInnerSchedulerId + i) % innerSchedulers.size());
+            end = start + onceSendSize + (i < remainder ? 1 : 0);
+            if (end == start) {
+                break;
+            }
+            innerScheduler.addInstance(instances.subList(start, end));
+            sentInnerSchedulers.add(innerScheduler);
+            start = end;
+        }
+
         lastInnerSchedulerId = (lastInnerSchedulerId + 1) % datacenter.getInnerSchedulers().size();
-        LOGGER.info("{}: {}'s LoadBalanceRound send {} instances to {}", datacenter.getSimulation().clockStr(), datacenter.getName(), instances.size(), innerScheduler.getName());
-        return List.of(innerScheduler);
+        LOGGER.info("{}: {}'s LoadBalanceRound send {} instances to {} innerSchedulers,On average, each scheduler receives around {} instances", datacenter.getSimulation().clockStr(), datacenter.getName(), instances.size(), sentInnerSchedulers.size(), onceSendSize);
+        return sentInnerSchedulers;
     }
 }
