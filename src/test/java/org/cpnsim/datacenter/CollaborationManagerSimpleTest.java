44819f07e13281fe302d70052763120f6e3dff5c
VVsxmja <vvsxmja@qq.com>
2023-07-21 07:36:33 +0800

    test(datacenters): fix testGetOtherDatacenters()
    将有序的List比较换成无序的Set比较

--- src/test/java/org/cpnsim/datacenter/CollaborationManagerSimpleTest.java
+++ src/test/java/org/cpnsim/datacenter/CollaborationManagerSimpleTest.java
@@ -4,6 +4,7 @@ import org.cloudsimplus.core.CloudSim;
 import org.cloudsimplus.core.Simulation;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Map;
@@ -54,9 +55,9 @@ public class CollaborationManagerSimpleTest {
         Datacenter dc2 = new DatacenterSimple(scaleCloudSim);
         Map<Integer, Set<Datacenter>> collaborationMap = Map.of(0, Set.of(dc1), 1, Set.of(dc0, dc2));
         collaborationManager.addDatacenter(collaborationMap);
-        List<Datacenter> excepted = List.of(dc1);
-        assertEquals(excepted, collaborationManager.getOtherDatacenters(dc0, 0));
-        List<Datacenter> excepted1 = List.of(dc1, dc2);
-        assertEquals(excepted1, collaborationManager.getOtherDatacenters(dc0));
+        Set<Datacenter> excepted = Set.of(dc1);
+        assertEquals(excepted, new HashSet<>(collaborationManager.getOtherDatacenters(dc0, 0)));
+        Set<Datacenter> excepted1 = Set.of(dc1, dc2);
+        assertEquals(excepted1, new HashSet<>(collaborationManager.getOtherDatacenters(dc0)));
     }
 }
