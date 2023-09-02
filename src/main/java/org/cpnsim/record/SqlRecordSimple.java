07080aa508d317eecce00ada2fb22bf6964747d8
魏鑫磊 <281796586@qq.com>
2023-09-03 00:18:22 +0800

    feat：add query code to SqlRecordSimple

--- src/main/java/org/cpnsim/record/SqlRecordSimple.java
+++ src/main/java/org/cpnsim/record/SqlRecordSimple.java
@@ -1,7 +1,6 @@
 package org.cpnsim.record;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.cpnsim.request.Instance;
 import org.cpnsim.request.InstanceGroup;
 import org.cpnsim.request.UserRequest;
@@ -12,8 +11,10 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class SqlRecordSimple implements SqlRecord {
     Logger LOGGER = LoggerFactory.getLogger(SqlRecordSimple.class.getSimpleName());
@@ -373,5 +374,62 @@ public class SqlRecordSimple implements SqlRecord {
                 " FOREIGN KEY(userRequestId) REFERENCES " + this.userRequestTableName + "(id))";
         stmt.executeUpdate(sql);
         conn.commit();
+    }
+
+    public Map<String,Map<String,Object>> query(String sql,String id1,String id2){
+        String label1=null,label2=null;
+        Map<String, Object> record = new HashMap<String, Object>();
+        Map<String, Map<String, Object>> recordMap = new HashMap<>();
+
+        try {
+            statement=conn.prepareStatement(sql);
+            ResultSet resultSet=statement.executeQuery();
+            // 获取元数据
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            for (int i=0; i < resultSetMetaData.getColumnCount(); i++){
+                String columnLabel = resultSetMetaData.getColumnLabel(i + 1);
+                if (Objects.equals(columnLabel,id1)){
+                    label1 = columnLabel;
+                }else if (Objects.equals(columnLabel,id2)){
+                    label2 = columnLabel;
+                }
+            }
+
+            // 打印一列的列名
+            while (resultSet.next()) {
+                //获取数据表中满足要求的一行数据，并放入Map中
+                for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                    String columnLabel = resultSetMetaData.getColumnLabel(i + 1);
+                    Object columnValue = resultSet.getObject(columnLabel);
+                    // System.out.println(columnLabel);
+                    record.put(columnLabel, columnValue);
+                }
+
+                String key;
+                if (label2==null){
+                    key=resultSet.getString(label1);
+                }else{
+                    key= resultSet.getString(label1)+"-"+resultSet.getString(label2);
+                }
+                recordMap.put(key,record);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return recordMap;
+    }
+
+    public Map<String,Map<String,Object>> queryUserRequestTable(){
+        return query("select * from "+this.userRequestTableName,"id",null);
+    }
+    public Map<String,Map<String,Object>> queryInstanceGroupTable(){
+        return query("select * from "+this.instanceGroupTableName,"id",null);
+    }
+    public Map<String,Map<String,Object>> queryInstanceGroupGraphTable(){
+        return query("select * from "+this.instanceGroupGraphTableName,"srcDcId","dstDcId");
+    }
+    public Map<String,Map<String,Object>> queryInstanceTable(){
+        return query("select * from "+this.instanceTableName,"id",null);
     }
 }
