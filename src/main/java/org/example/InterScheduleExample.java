8696a8b5ad2c1562594731f11246dd5089563e31
huahuadan <ahuahuadan@qq.com>
2023-10-10 09:49:51 +0800

    feat(example):add an example for InterSchedule

---
+++ src/main/java/org/example/InterScheduleExample.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import ch.qos.logback.classic.Level;
+import org.cloudsimplus.core.CloudSim;
+import org.cloudsimplus.core.Factory;
+import org.cloudsimplus.core.FactorySimple;
+import org.cloudsimplus.core.Simulation;
+import org.cloudsimplus.network.topologies.BriteNetworkTopology;
+import org.cloudsimplus.util.Log;
+import org.cpnsim.datacenter.Datacenter;
+import org.cpnsim.datacenter.InitDatacenter;
+import org.cpnsim.record.MemoryRecord;
+import org.cpnsim.user.UserRequestManager;
+import org.cpnsim.user.UserRequestManagerCsv;
+import org.cpnsim.user.UserSimple;
+
+public class InterScheduleExample {
+    Simulation cpnSim;
+    Factory factory;
+    UserSimple user;
+    UserRequestManager userRequestManager;
+    String NETWORK_TOPOLOGY_FILE = "./src/main/resources/experiment/interScheduleExperiment/topology.brite";
+    //    String DATACENTER_CONFIG_FILE = "./src/main/resources/experiment/interScheduleExperiment/DatacentersConfig.json";
+    String DATACENTER_CONFIG_FILE = "./src/main/resources/experiment/interScheduleExperiment/DatacentersConfigDistributeDirect.json";
+    String USER_REQUEST_FILE = "./src/main/resources/experiment/interScheduleExperiment/generateRequestParament.csv";
+
+    public static void main(String[] args) {
+        new InterScheduleExample();
+    }
+
+    private InterScheduleExample() {
+        double start = System.currentTimeMillis();
+        Log.setLevel(Level.INFO);
+        cpnSim = new CloudSim();
+        factory = new FactorySimple();
+        initUser();
+        initDatacenters();
+        initNetwork();
+        double endInit = System.currentTimeMillis();
+        cpnSim.start();
+        double end = System.currentTimeMillis();
+        System.out.println("\n运行情况：");
+        System.out.println("初始化耗时：" + (endInit - start) / 1000 + "s");
+        System.out.println("模拟运行耗时：" + (end - endInit) / 1000 + "s");
+        System.out.println("模拟总耗时：" + (end - start) / 1000 + "s");
+        System.out.println("运行过程占用最大内存: " + MemoryRecord.getMaxUsedMemory() / 1000000 + " Mb");
+        System.out.println("运行结果保存路径:" + cpnSim.getSqlRecord().getDbPath());
+    }
+
+    private void initUser() {
+        userRequestManager = new UserRequestManagerCsv(USER_REQUEST_FILE);
+        user = new UserSimple(cpnSim, userRequestManager);
+    }
+
+    private void initDatacenters() {
+        InitDatacenter.initDatacenters(cpnSim, factory, DATACENTER_CONFIG_FILE);
+    }
+
+    private void initNetwork() {
+        BriteNetworkTopology networkTopology = BriteNetworkTopology.getInstance(NETWORK_TOPOLOGY_FILE);
+        networkTopology.mapNode(cpnSim.getCis(), 0);
+        cpnSim.setNetworkTopology(networkTopology);
+        for (int collabId : cpnSim.getCollaborationManager().getCollaborationIds()) {
+            for (Datacenter datacenter : cpnSim.getCollaborationManager().getDatacenters(collabId)) {
+                networkTopology.mapNode(datacenter, datacenter.getId());
+            }
+        }
+    }
+}
