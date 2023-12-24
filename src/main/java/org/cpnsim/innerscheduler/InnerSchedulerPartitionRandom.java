ebb220e080aa9c541ba7210f563532fefbf031cd
huahuadan <ahuahuadan@qq.com>
2023-12-24 21:50:10 +0800

    fix(innerScheduler):fix InnerSchedulerPartitionRandom

--- src/main/java/org/cpnsim/innerscheduler/InnerSchedulerPartitionRandom.java
+++ src/main/java/org/cpnsim/innerscheduler/InnerSchedulerPartitionRandom.java
@@ -1,12 +1,14 @@
 package org.cpnsim.innerscheduler;
 
 import org.cpnsim.request.Instance;
+import org.cpnsim.statemanager.HostState;
 import org.cpnsim.statemanager.SynState;
 
 import java.util.*;
 
 public class InnerSchedulerPartitionRandom extends InnerSchedulerSimple {
     Random random = new Random();
+    long excludeTimeNanos = 0;
 
     public InnerSchedulerPartitionRandom(int id, int firstPartitionId, int partitionNum) {
         super(id, firstPartitionId, partitionNum);
@@ -14,22 +16,27 @@ public class InnerSchedulerPartitionRandom extends InnerSchedulerSimple {
 
     @Override
     protected InnerSchedulerResult scheduleInstances(List<Instance> instances, SynState synState) {
+        excludeTimeNanos = 0;
         InnerSchedulerResult innerSchedulerResult = new InnerSchedulerResult(this, getDatacenter().getSimulation().clock());
 
+        int synPartitionId = firstPartitionId;
+        if (datacenter.getStatesManager().isSynCostTime()) {
+            synPartitionId = (firstPartitionId + datacenter.getStatesManager().getSmallSynGapCount()) % partitionNum;
+        }
         for (Instance instance : instances) {
             int suitId = -1;
 
-            int synPartitionId = firstPartitionId;
-            if (datacenter.getStatesManager().isSynCostTime()) {
-                synPartitionId = (firstPartitionId + datacenter.getStatesManager().getSmallSynGapCount()) % partitionNum;
-            }
             for (int p = 0; p < partitionNum; p++) {
                 int[] range = datacenter.getStatesManager().getPartitionRangesManager().getRange((synPartitionId + partitionNum - p) % partitionNum);
                 int startHostId = random.nextInt(range[1] - range[0] + 1);
                 int rangeLength = range[1] - range[0] + 1;
                 for (int i = 0; i < rangeLength; i++) {
                     int hostId = range[0] + (startHostId + i) % rangeLength;
-                    if (synState.isSuitable(hostId, instance)) {
+                    long startTime = System.nanoTime();
+                    HostState hostState = synState.getHostState(hostId);
+                    long endTime  = System.nanoTime();
+                    excludeTimeNanos += endTime - startTime;
+                    if (hostState.isSuitable(instance)) {
                         suitId = hostId;
                         break;
                     }
@@ -48,6 +55,7 @@ public class InnerSchedulerPartitionRandom extends InnerSchedulerSimple {
             }
         }
 
+        excludeTime = excludeTimeNanos/1_000_000;
         return innerSchedulerResult;
     }
 }
