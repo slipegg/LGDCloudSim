fab4fabbcdd0e1ea8625b2f81e2ef77c75e20102
huahuadan <ahuahuadan@qq.com>
2024-03-17 16:30:24 +0800

    refactor(util):add code comment for util module.

--- src/main/java/org/cpnsim/util/GoogleTraceRequestFile.java
+++ src/main/java/org/cpnsim/util/GoogleTraceRequestFile.java
@@ -3,17 +3,36 @@ package org.cpnsim.util;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * A class that represents the Google trace request file.
+ * It contains the google trace request CSV file path, the area where the request is from, and the row number of the file.
+ *
+ * @author Jiawen Liu
+ * @since LGDCloudSim 1.0
+ */
+@Getter
+@Setter
 public class GoogleTraceRequestFile {
-    @Setter
-    @Getter
+    /**
+     * The google trace request CSV file path.
+     */
     private String filePath;
-    @Setter
-    @Getter
+    /**
+     * The area where the request is from.
+     */
     private String area;
-    @Setter
-    @Getter
+    /**
+     * The row number of the file.
+     */
     private Integer rowNum;
 
+    /**
+     * Construct the google trace request file with the file path, the area, and the row number.
+     *
+     * @param filePath the google trace request CSV file path.
+     * @param area     the area where the request is from.
+     * @param rowNum   the row number of the file.
+     */
     public GoogleTraceRequestFile(String filePath, String area, Integer rowNum) {
         this.filePath = filePath;
         this.area = area;
