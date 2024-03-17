c8913e3f34f9a40ea88be94d2f477971ca5e59f9
huahuadan <ahuahuadan@qq.com>
2024-03-17 16:30:11 +0800

    refactor(intrascheduler):add code comment for intrascheduler module.

--- src/main/java/org/cpnsim/intrascheduler/IntraSchedulerResult.java
+++ src/main/java/org/cpnsim/intrascheduler/IntraSchedulerResult.java
@@ -9,23 +9,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * The result of the intra-scheduler scheduling.
+ * It contains the intra-scheduler, the scheduled instances, the schedule time, and the failed instances.
+ *
+ * @author Jiawen Liu
+ * @since LGDCloudSim 1.0
+ */
+@Getter
 public class IntraSchedulerResult {
-    @Getter
+    /**
+     * The intra-scheduler that schedules the instances.
+     */
     private IntraScheduler intraScheduler;
 
-    @Getter
+    /**
+     * The scheduled instances.
+     * The schedule result is stored in the {@link Instance#getExpectedScheduleHostId()} field.
+     */
     private List<Instance> scheduledInstances;
 
-    @Getter
+    /**
+     * The time spent on the scheduling.
+     */
     private double scheduleTime;
 
-    @Getter
+    /**
+     * The failed instances.
+     */
     private List<Instance> failedInstances;
 
-    @Getter
+    /**
+     * The out-dated user requests.
+     */
     @Setter
     private Set<UserRequest> outDatedUserRequests;
 
+    /**
+     * Construct the intra-scheduler result with the intra-scheduler and the schedule time.
+     *
+     * @param intraScheduler the intra-scheduler that schedules the instances.
+     * @param scheduleTime   the time spent on the scheduling.
+     */
     public IntraSchedulerResult(IntraScheduler intraScheduler, double scheduleTime) {
         this.intraScheduler = intraScheduler;
         this.scheduleTime = scheduleTime;
@@ -33,22 +58,42 @@ public class IntraSchedulerResult {
         this.failedInstances = new ArrayList<>();
     }
 
+    /**
+     * Add the scheduled instance to the scheduled instances list.
+     * @param instance the scheduled instance.
+     */
     public void addScheduledInstance(Instance instance) {
         this.scheduledInstances.add(instance);
     }
 
+    /**
+     * Add the failed instance to the failed instances list.
+     * @param instance the failed instance.
+     */
     public void addFailedScheduledInstance(Instance instance) {
         this.failedInstances.add(instance);
     }
 
+    /**
+     * Get the number of the scheduled instances and the failed instances.
+     * @return the number of the scheduled instances and the failed instances.
+     */
     public int getInstanceNum() {
         return scheduledInstances.size() + failedInstances.size();
     }
 
+    /**
+     * Get whether the scheduled instances list is empty.
+     * @return whether the scheduled instances list is empty.
+     */
     public boolean isScheduledInstancesEmpty() {
         return scheduledInstances.isEmpty();
     }
 
+    /**
+     * Get whether the failed instances list is empty.
+     * @return whether the failed instances list is empty.
+     */
     public boolean isFailedInstancesEmpty() {
         return failedInstances.isEmpty() && outDatedUserRequests.isEmpty();
     }
