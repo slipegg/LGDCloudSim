8bb1cd6d88c754907e3b4d1696385c920de23e1c
huahuadan <ahuahuadan@qq.com>
2024-04-06 14:50:37 +0800

    fix(test): fix some test files.

--- src/test/java/org/lgdcloudsim/user/UserRequestManagerGoogleTraceTest.java
+++ src/test/java/org/lgdcloudsim/user/UserRequestManagerGoogleTraceTest.java
@@ -31,19 +31,19 @@ public class UserRequestManagerGoogleTraceTest {
         int INSTANCE_GROUP_RETRY_TIMES = 0;
         int INSTANCE_RETRY_TIMES = 0;
         Map<Integer, GoogleTraceRequestFile> GOOGLE_TRACE_REQUEST_FILE_DC_MAP = new HashMap<>() {{
-            put(1, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_a_user_requests.csv", "United States", 5000));
-            put(2, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_b_user_requests.csv", "United States", 5000));
-            put(3, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_c_user_requests.csv", "United States", 5000));
-            put(4, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_d_user_requests.csv", "United States", 5000));
-            put(5, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_e_user_requests.csv", "United States", 5000));
-            put(6, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_f_user_requests.csv", "United States", 5000));
-            put(7, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_g_user_requests.csv", "United States", 5000));
-            put(8, new GoogleTraceRequestFile("./src/main/resources/experiment/googleTrace/userRequest/2019_h_user_requests.csv", "United States", 5000));
+            put(1, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_a_user_requests.csv", "United States", 5000));
+            put(2, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_b_user_requests.csv", "United States", 5000));
+            put(3, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_c_user_requests.csv", "United States", 5000));
+            put(4, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_d_user_requests.csv", "United States", 5000));
+            put(5, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_e_user_requests.csv", "United States", 5000));
+            put(6, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_f_user_requests.csv", "United States", 5000));
+            put(7, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_g_user_requests.csv", "United States", 5000));
+            put(8, new GoogleTraceRequestFile("./src/main/resources/example/GoogleTrace/userRequest/2019_h_user_requests.csv", "United States", 5000));
         }};
         UserRequestManagerGoogleTrace userRequestManager = new UserRequestManagerGoogleTrace(GOOGLE_TRACE_REQUEST_FILE_DC_MAP, MAX_CPU_CAPACITY, MAX_RAM_CAPACITY, STORAGE_CAPACITY, BW_CAPACITY, LIFE_TIME_MEAN, LIFE_TIME_STD, INSTANCE_GROUP_RETRY_TIMES, INSTANCE_RETRY_TIMES,
                 ACCESS_LATENCY_PERCENTAGE, ACCESS_LATENCY_MEAN, ACCESS_LATENCY_STD, IS_EDGE_DIRECTED, EDGE_PERCENTAGE, EDGE_DELAY_MEAN, EDGE_DELAY_STD, EDGE_BW_MEAN, EDGE_BW_STD);
 
         Map<Integer, List<UserRequest>> userRequests = userRequestManager.generateOnceUserRequests();
-        assertNotNull(userRequests);
+        assert (!userRequests.isEmpty());
     }
 }
